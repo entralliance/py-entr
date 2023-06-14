22d0e63ea513419101a1b6c0944c32014ef01fd8
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-06-14 15:08:58 -0400

    fix asset_id case sensitivity

--- entr/plantdata.py
+++ entr/plantdata.py
@@ -160,7 +160,8 @@ def load_openoa_rpt_table_tag_metadata(conn:EntrConnection, plant_name:str, tabl
 
         metadata = {
             "frequency": pd.Timedelta(seconds=float(df["INTERVAL_S"][0])),
-            "time": "TIME"
+            "time": "TIME",
+            "asset_id": "ASSET_ID"
         }
     else:
         assert len(df["interval_s"].unique())==1, "Not all tags are at the same time resolution."
